e6fad1bcd4921f6e9076f5a72e66d844f4cbd3d0
unknown <RizzoM@NRC016.hq.un.fao.org>
2017-02-01 15:58:51 +0100

    new version of open tenure on android studio

---
+++ openTenure/build/generated/source/buildConfig/debug/org/fao/sola/clients/android/opentenure/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.fao.sola.clients.android.opentenure;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.fao.sola.clients.android.opentenure";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 22;
+  public static final String VERSION_NAME = "1.8.3";
+}
